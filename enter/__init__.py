cccae419412b7c92ee8db72a2b0698014eda236f
johnslavik <bartosz@ilikepython.com>
2026-01-22 09:30:15 +0100

    Better naming

--- enter/__init__.py
+++ enter/__init__.py
@@ -20,16 +20,12 @@ class Context[C, **P]:
     1
     """
 
-    def __init__(
-        self,
-        constructor: type[C],
-        contextvar: ContextVar[C],
-    ) -> None:
+    def __init__(self, constructor: Callable[P, C], contextvar: ContextVar[C]) -> None:
         self.constructor = constructor
         self.contextvar = contextvar
 
     @contextmanager
-    def __call__(self, *_: P.args, **kwargs: P.kwargs) -> Generator[C]:
+    def __call__(self, *__never__: P.args, **kwargs: P.kwargs) -> Generator[C]:
         value = self.constructor(**kwargs)
         token = self.contextvar.set(value)
         try:
@@ -38,19 +34,19 @@ class Context[C, **P]:
             token.var.reset(token)
 
 
-class ContextDescriptor:
+class _ContextGetter:
     def __get__[T, **P](self, instance: T, owner: Callable[P, T]) -> Context[T, P]:
         return context_of(owner or type(instance))
 
 
-class CurrentDescriptor:
+class _CurrentInstanceGetter:
     def __get__[T: ContextClass, **P](
         self,
         instance: T | None,
         owner: Callable[P, T],
     ) -> T:
-        cls = owner if instance is None else instance
-        return cls.context.contextvar.get()
+        lens = owner if instance is None else instance
+        return lens.context.contextvar.get()
 
 
 @cache
@@ -60,18 +56,9 @@ def context_of[T, **P](context_class: Callable[P, T]) -> Context[T, P]:
 
 @dataclass_transform(kw_only_default=True)
 class ContextClass:
-    context: ClassVar[ContextDescriptor] = ContextDescriptor()
-    current: ClassVar[CurrentDescriptor] = CurrentDescriptor()
+    context: ClassVar[_ContextGetter] = _ContextGetter()
+    current: ClassVar[_CurrentInstanceGetter] = _CurrentInstanceGetter()
 
     def __init_subclass__(cls, **kwargs: Any) -> None:
         kwargs["kw_only"] = True
         dataclass(cls, **kwargs)
-
-
-if __name__ == "__main__":
-
-    class Foo(ContextClass):
-        x: int | None = None
-
-    with Foo.context(x=1):
-        reveal_type(Foo.current.x)
